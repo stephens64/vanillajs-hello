f0b7851dc075c41bc79c04950223202d09d0d6a7
stephens64 <96099094+stephens64@users.noreply.github.com>
2024-07-14 02:24:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["My Dog ", "My Uncle Joe ", "The Gods ", "Some Stranger "];
+let action = ["ate ", "blew up ", "crushed ", "broke "];
+let what = ["my homework ", "my phone ", "the car "];
+let when = [
+  "before the class ",
+  "when I was sleeping ",
+  "while I was exercising ",
+  "during my lunch ",
+  "while I was praying "
+];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello from the console!");
+  document.body.innerHTML =
+    getRandom(who) + getRandom(action) + getRandom(what) + getRandom(when);
 };
